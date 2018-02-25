e48be8a76d9ceaf18ec5d8731f548f884d1f33af
Aravind Putrevu <aravind.putrevu@elastic.co>
2018-02-25 14:11:38 -0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r3h^bx1_!$#%wxf-g5$zt6=b)gps2+-aq+(56a!b^$!pu5dsj0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
